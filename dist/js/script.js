f8c65be10721f47fc755799d0d1081bd9fa5da00
pless <iliaplesovskikh@gmail.com>
2023-03-07 20:43:49 +0600

    Added portfolio, added fish text. Next step add mailer

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,9 +10,35 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+const promo = document.querySelector('.openModal'),
+    promoMenu = document.querySelector('.promo-menu'),
+    closeMenu = document.querySelector('.promo-menu__close');
+
+promo.addEventListener('click', () => {
+    promoMenu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+    promoMenu.classList.remove('active');
+});
+
+
 const procents = document.querySelectorAll('.skills__ratings-counter'),
     lines = document.querySelectorAll('.skills__ratings-line span');
 
 procents.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+const smoothLinks = document.querySelectorAll("a[href^='#']");
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute("href");
+
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+};
